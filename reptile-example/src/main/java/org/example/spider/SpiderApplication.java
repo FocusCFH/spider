72105f27241d17d07f5827a05ea1f2b8e815522f
chenfuhao <577062621@qq.com>
2021-09-15 10:31:31 +0800

    fix：集成springboot

---
+++ reptile-example/src/main/java/org/example/spider/SpiderApplication.java
@@ -0,0 +1,18 @@
+package org.example.spider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName SpiderApplication
+ * @Description
+ * @Author chenfuhao
+ * @Date 2021/9/15 9:30
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class SpiderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpiderApplication.class,args);
+    }
+}
